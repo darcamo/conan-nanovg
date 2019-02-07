9b1e371a16a578dfb4bc4f1f5b98e1ed2192bee7
Darlan Cavalcante Moreira <darcamo@gmail.com>
2019-02-06 22:27:57 -0300

    Use a better name for the library version
    Since nanovg has no release, there is no "version" and we use a specific commit.
    Now, for a version in conan, we use the date of the last commit in master and
    its short sha1.

--- conanfile.py
+++ conanfile.py
@@ -4,9 +4,9 @@ import shutil
 import glob
 
 
-class ImguisfmlConan(ConanFile):
+class NanovgConan(ConanFile):
     name = "nanovg"
-    version = "f4069e6a1ad5da430fb0a9c57476d5ddc2ff89b2"
+    version = "2018-09-27-f4069e6"
     license = "https://raw.githubusercontent.com/memononen/nanovg/master/LICENSE.txt"
     author = "Darlan Cavalcante Moreira (darcamo@gmail.com)"
     url = "https://github.com/darcamo/conan-nanovg"
@@ -18,13 +18,13 @@ class ImguisfmlConan(ConanFile):
     generators = "cmake"
     exports_sources = "CMakeLists.txt"
     requires = "glfw/3.2.1@bincrafters/stable"
+    homepage = "https://github.com/memononen/nanovg.git"
 
     def source(self):
-        git = tools.Git(folder="nanovg")
-
-        git.clone("https://github.com/memononen/nanovg.git")
-        git.checkout(self.version)
-        os.rename("nanovg".format(self.version), "sources")
+        git = tools.Git(folder="sources")
+        commit_sha1 = self.version.split("-")[-1]
+        git.clone(self.homepage)
+        git.checkout(commit_sha1)
 
         # Copy the CMakeLists.txt file to the sources folder
         shutil.move("CMakeLists.txt", "sources/")
